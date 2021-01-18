cf13ed7fb5c8d78ef937c884b77590a5438c5d1f
Jhonatan Teixeira <jhonatan.teixeira@guararapes.ind.br>
2021-01-17 21:41:52 -0300

    making compras service run

---
+++ install.sh
@@ -0,0 +1,23 @@
+cd compras
+npm install
+cd ..
+
+cd items
+npm install
+cd ..
+
+cd pagamentos
+npm install
+cd ..
+
+cd relatorios
+npm install
+cd ..
+
+cd relatorio-integration
+npm install
+cd ..
+
+cd usuarios
+npm install
+cd ..
